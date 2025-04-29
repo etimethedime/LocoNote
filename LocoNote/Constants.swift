82ccfc768693653d943006f326cf9e46bce13155
ezra <ezra54311@gmail.com>
2025-04-29 16:36:39 -0400

    constants class created- e

--- LocoNote/Constants.swift
+++ LocoNote/Constants.swift
@@ -1,3 +1,9 @@
 
 
 import Foundation
+
+struct Constants{
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "kSortDirectionAscending"
+    static let kSortDirectionDescending = "sortDirectionDescending"
+}
