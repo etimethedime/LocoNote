1092749d09240d094dcf16497ff95e2955233e9a
ezra <ezra54311@gmail.com>
2025-04-29 18:54:20 -0400

    app delegate settings method added - e

--- LocoNote/AppDelegate.swift
+++ LocoNote/AppDelegate.swift
@@ -35,6 +35,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let settings = UserDefaults.standard
+        
+        if settings.string(forKey: Constants.kSortField) == nil{
+            settings.set("city", forKey: Constants.kSortField)
+        }
+        if settings.string(forKey: Constants.kSortDirectionAscending) == nil{
+            settings.set(false, forKey: Constants.kSortDirectionAscending)
+        }
+        if settings.string(forKey:Constants.kSortDirectionDescending) == nil{
+            settings.set(true, forKey: Constants.kSortDirectionDescending)
+        }
+        settings.synchronize()
+        NSLog("Sort field: %@", settings.string(forKey: Constants.kSortField)!)
+        NSLog("Sort direction: \(settings.bool (forKey: Constants.kSortDirectionAscending))")
         return true
     }
 
